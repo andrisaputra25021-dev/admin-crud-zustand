faa9aeba3bc252d5c099acc6de78c0c766347038
Andri Saputra <andri.saputra25021@gmail.com>
2026-05-16 15:17:12 +0700

    fix: add basename to BrowserRouter for Github Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/admin-crud-zustand">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/admin-crud-zustand",
   plugins: [react(), tailwindcss()],
 });
